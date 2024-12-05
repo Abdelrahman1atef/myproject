f3c6c6987d62eae9d2d473964a8c0030c220e7a2
Abdelrahman Atef <man1207468@gmail.com>
2024-12-05 13:51:52 +0200

    v0.3

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 # api/urls.py
 from django.urls import path
 from .views import ProductListView
+from .views import fetch_image_view
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('fetch-image/', fetch_image_view, name='fetch_image'),
 ]
