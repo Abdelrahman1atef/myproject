a5d41642de61260d030a9e5ec8c11248c8c5c62f
Abdelrahman Atef <man1207468@gmail.com>
2024-12-10 23:12:12 +0200

    V0.4

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 # api/urls.py
 from django.urls import path
 from .views import ProductListView
-from .views import fetch_image_view
+from .views import ProductListByCompanyView
+from .views import ProductListByGroupView
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('fetch-image/', fetch_image_view, name='fetch_image'),
+    path('allproducts/', ProductListView.as_view(), name='product-list'),
+    path('productsByCompany_id/<int:company_id>/', ProductListByCompanyView.as_view(), name='product-list-by-company'),
+    path('productsByGroup_id/<int:group_id>/', ProductListByGroupView.as_view(), name='product-list-by-group'),
 ]
